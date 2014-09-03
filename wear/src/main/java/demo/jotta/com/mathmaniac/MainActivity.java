b24b3a205050189fdb6215faf080b194a8143e79
jorge.reyes <jorge.reyes@globant.com>
2014-09-03 17:14:35 -0500

    Game timer fixed, now you can recover a life if you do 5 operations right in a row, several fixes.

--- wear/src/main/java/demo/jotta/com/mathmaniac/MainActivity.java
+++ wear/src/main/java/demo/jotta/com/mathmaniac/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends Activity {
 
 
     private TextView mNumber;
-    private ProgressBar mProgressBar;
     private TextView mOperation;
     private RelativeLayout mRelativeLayout;
 
@@ -33,16 +32,18 @@ public class MainActivity extends Activity {
     private ImageView mLife1;
     private ImageView mLife2;
     private ImageView mLife3;
+    private boolean isHeartOneVisible = true;
+    private boolean isHeartTwoVisible = true;
+    private boolean isHeartThreeVisible = true;
     private int lives = 3;
-    private int secondsCounter = 10;
+    private long secondsCounter;
     int operation=0;
-    public  int max = 10000;
+    private int extraLives=0;
+    long extraSeconds = 0;
+    public  int maxTime = 20000;
     private CountDownTimer mCountDownTimer;
     private Context mContext = this;
-    private static final String  PATH_ROBOTO_BOLD = "fonts/Roboto-Bold.ttf";
-    private static int BOLD_ID = 1;
 
-    Typeface typeRobotoBold = Typeface.createFromAsset(getAssets(), PATH_ROBOTO_BOLD);
 
 
     @Override
@@ -61,13 +62,11 @@ public class MainActivity extends Activity {
                 mLife2 = (ImageView) stub.findViewById(R.id.img_life2);
                 mLife3 = (ImageView) stub.findViewById(R.id.img_life3);
 
-                mNumber.setTypeface(typeRobotoBold);
-                mOperation.setTypeface(typeRobotoBold);
-
+//
                 //Initialize Game
                 mNumber.setText(factory());
                 livesCounterRefresher(lives);
-                gameTimer(max);
+                gameTimer(maxTime);
 
             }
         });
@@ -75,20 +74,22 @@ public class MainActivity extends Activity {
 
 
 
-    public void gameTimer(int max){
-        secondsCounter = max/1000;
+    public void gameTimer(long max){
+
         mCountDownTimer = new CountDownTimer(max,1000) {
 
             @Override
             public void onTick(long millisUntilFinished) {
-                secondsCounter--;
-                mTimer.setText(Integer.toString(secondsCounter));
+                    secondsCounter = millisUntilFinished/1000;
+                    extraSeconds = secondsCounter;
+                    mTimer.setText("Remaining: " + Long.toString(secondsCounter));
             }
 
             @Override
             public void onFinish() {
                 if(lives >= 0){
                     lifeCounter(lives);
+                    gameTimer(5000);
                 }else if (lives<0){
                     Toast.makeText(mContext, "You Lose!!" +" " + ("\ud83d\ude1e"), Toast.LENGTH_SHORT).show();
                     mRelativeLayout.removeAllViews();
@@ -110,12 +111,15 @@ public class MainActivity extends Activity {
         int High = 100;
         int generatedNumber = random.nextInt(High-Low) + Low;
 
+        mOperation.setText(String.valueOf(0));
+
         return String.valueOf(generatedNumber);
+
 
     }
 
 
-    public int operation(View view){
+    public void operation(View view){
 
         int sum =0;
 
@@ -137,33 +141,52 @@ public class MainActivity extends Activity {
                 mOperation.setText(Integer.toString(operation));
                 break;
         }
+
         operation += sum;
         mOperation.setText(Integer.toString(operation));
 
-        if(operation == Integer.parseInt(mOperation.getText().toString())){
-            gameTimer(6000);
+        String actualValue = mNumber.getText().toString();
+
+        if(actualValue.toString().equals(mNumber.getText().toString())){
+            if(operation == Integer.parseInt(mNumber.getText().toString())){
+                extraLives++;
+
+                if(extraLives >5){
+                    if(lives <=3){
+                        lives++;
+                        rewardLives();
+                        extraLives = 0;
+                    }
+                }
+
+                mOperation.setText(String.valueOf(0));
+                operation = 0;
+                mNumber.setText(factory());
+                long extraTime = (extraSeconds * 1000) + 5000;
+                gameTimer(extraTime);
+            }else if (operation > Integer.parseInt(mNumber.getText().toString())){
+                operation = 0;
+                mOperation.setText(Integer.toString(operation));
+                mNumber.setText(factory());
+            }
         }else{
-            operation = 0;
+            operation =0;
             mOperation.setText(Integer.toString(operation));
             mNumber.setText(factory());
-        }
 
-        return sum;
+        }
     }
 
     private int lifeCounter(int livesCount){
         if(lives == 0){
             lives = -1;
-            max = 1;
-            gameTimer(max);
+            maxTime = 1;
+            gameTimer(maxTime);
         }else{
             livesCount --;
             lives = livesCount;
             livesCounterRefresher(lives);
             mNumber.setText(factory());
-            max = 4000;
-
-            gameTimer(max);
         }
         return lives;
     }
@@ -171,10 +194,31 @@ public class MainActivity extends Activity {
     private void livesCounterRefresher(int livesCount){
         if(livesCount == 2){
             mLife3.setVisibility(View.INVISIBLE);
+            isHeartThreeVisible = false;
         }else if(livesCount == 1){
             mLife2.setVisibility(View.INVISIBLE);
+            isHeartTwoVisible = false;
         }else if(livesCount == 0){
             mLife1.setVisibility(View.INVISIBLE);
+            isHeartOneVisible = false;
+        }
+    }
+
+    private void rewardLives(){
+        lives++;
+
+        if(!isHeartThreeVisible && isHeartTwoVisible && isHeartOneVisible){
+            mLife3.setVisibility(View.VISIBLE);
+            lives++;
+            isHeartThreeVisible = true;
+        }else if(!isHeartTwoVisible && isHeartOneVisible){
+            mLife2.setVisibility(View.VISIBLE);
+            lives++;
+            isHeartTwoVisible = true;
+        }else if(!isHeartOneVisible && !isHeartTwoVisible && !isHeartThreeVisible){
+            mLife1.setVisibility(View.VISIBLE);
+            lives++;
+            isHeartOneVisible = true;
         }
     }
 
